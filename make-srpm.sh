24c63b8991851f2bc188cfdbfe0cf230936d1a41
Fabio Valentini <fafatheone@gmail.com>
2015-06-12 08:20:28 +0200

    Add bash script for building RPM source package.

---
+++ make-srpm.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+VERSION=0.0.1
+
+mkdir -p $HOME/rpmbuild/SOURCES
+mkdir -p $HOME/rpmbuild/SPECS
+
+git archive --format=tar.gz --prefix=enerpy-$VERSION/ HEAD > $HOME/rpmbuild/SOURCES/enerpy-$VERSION.tar.gz
+
+cp ./enerpy.spec $HOME/rpmbuild/SPECS/
+
+cd $HOME/rpmbuild/SPECS/
+rpmbuild -bs enerpy.spec
+
+cd $HOME/rpmbuild/SRPMS/
+mv *.rpm $HOME/
+
+cd $HOME
+rm -r rpmbuild
