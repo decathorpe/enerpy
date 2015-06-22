3f21473d76061531865eb5afa40c0114871ba831
Fabio Valentini <fafatheone@gmail.com>
2015-06-22 20:52:46 +0200

    Implement basic functions for lists of ints, lists of floats and lists of Nums.

---
+++ enerpy/lists.py
@@ -0,0 +1,35 @@
+import math
+
+from enerpy.base import *
+from enerpy.calc import *
+from enerpy.functions import *
+
+class NumList(Node, list):
+	def __init__(self):
+		super().__init__()
+
+	def condense(self):
+		c = Num()
+		n = len(self)
+		
+		list_sumn = 0
+		for i in self:
+			list_sumn = list_sumn + coerce(i).val
+		
+		list_mean = list_sumn / n
+		c.val = list_mean
+		
+		list_var = 0
+		for i in self:
+			list_var = list_var + math.pow(list_mean - coerce(i).val, 2)
+		
+		valu_var = 0
+		for i in self:
+			valu_var = valu_var + coerce(i).var
+		
+		mean_vars = (1 / (n * (n - 1))) * list_var
+		valu_vars = (1 / math.pow(n, 2)) * valu_var
+		
+		c.var = mean_vars + valu_vars
+		
+		return c
