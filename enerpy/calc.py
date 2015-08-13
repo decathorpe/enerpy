354611ad55d29b8a0a82d274646b52c5f1446121
Fabio Valentini <decathorpe@gmail.com>
2015-08-13 14:47:25 +0200

    spring-cleaning of enerpy/calc.py - added back basic arithmetic functions (add, sub, mul, div, pwr) - added docstrings to classes, methods, functions - fixed line lengths - fixed indentation levels - renamed a few variables - in general: make pylint happy (10.00/10)

--- enerpy/calc.py
+++ enerpy/calc.py
@@ -1,112 +1,249 @@
+"""
+enerpy.base.py:
+This module contains all the math neccessary.
+Arguments are expected to be Num objects.
+"""
+
 import math
 
-from enerpy.base import *
+from enerpy.base import Num
 
-# Exponents and logarithms
-def exp(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.exp(a.val)
-    c.var = math.pow(c.val, 2) * a.var
-    return c
 
-def log(a, b):
-    assert isinstance(a, Num)
-    assert isinstance(b, Num)
-    c = Num()
-    c.val = math.log(a.val, b.val)
-    c.var = math.pow(c.val, 2) * ( (a.var) / (math.pow(a.val, 2) * math.pow(math.log(a.val), 2)) + (b.var) / (math.pow(b.val, 2) * math.pow(math.log(b.val), 2)) )
-    return c
+# Add, subtract
+def add(arg1, arg2):
+    """
+    enerpy.add:
+    Function to add two Nums.
+    """
+    assert isinstance(arg1, Num)
+    assert isinstance(arg2, Num)
+    result = Num()
+    result.val = arg1.val + arg2.val
+    result.var = arg1.var + arg2.var
+    return result
+
+def sub(arg1, arg2):
+    """
+    enerpy.sub:
+    Function to subtract two Nums.
+    """
+    assert isinstance(arg1, Num)
+    assert isinstance(arg2, Num)
+    result = Num()
+    result.val = arg1.val - arg2.val
+    result.var = arg1.var + arg2.var
+    return result
+
+
+# Multiply, divide
+def mul(arg1, arg2):
+    """
+    enerpy.mul:
+    Function to multiply two Nums.
+    """
+    assert isinstance(arg1, Num)
+    assert isinstance(arg2, Num)
+    result = Num()
+    result.val = arg1.val * arg2.val
+    result.var = math.pow(result.val, 2) * \
+            ((arg1.var / (math.pow(arg1.val, 2))) + \
+            (arg2.var / (math.pow(arg2.val, 2))))
+    return result
+
+def div(arg1, arg2):
+    """
+    enerpy.div:
+    Function to divide two Nums.
+    """
+    assert isinstance(arg1, Num)
+    assert isinstance(arg2, Num)
+    result = Num()
+    result.val = arg1.val / arg2.val
+    result.var = math.pow(result.val, 2) * \
+            ((arg1.var / (math.pow(arg1.val, 2))) + \
+            (arg2.var / (math.pow(arg2.val, 2))))
+    return result
+
+
+# Powers, logarithms
+def pwr(arg1, arg2):
+    """
+    enerpy.pow:
+    Function to return a to the power b.
+    """
+    assert isinstance(arg1, Num)
+    assert isinstance(arg2, Num)
+    result = Num()
+    result.val = math.pow(arg1.val, arg2.val)
+    result.var = math.pow(result.val, 2) * \
+                (math.pow(arg2.val/arg1.val, 2) * \
+                arg1.var + math.pow(math.log(arg1.val), 2) * arg2.var)
+    return result
+
+def exp(arg1):
+    """
+    enerpy.exp:
+    Function to return e to the a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.exp(arg1.val)
+    result.var = math.pow(result.val, 2) * arg1.var
+    return result
+
+def log(arg1, arg2):
+    """
+    enerpy.log:
+    Function to return log a, base b.
+    """
+    assert isinstance(arg1, Num)
+    assert isinstance(arg2, Num)
+    result = Num()
+    result.val = math.log(arg1.val, arg2.val)
+    result.var = math.pow(result.val, 2) * \
+            ((arg1.var) / (math.pow(arg1.val, 2) * math.pow(math.log(arg1.val), 2)) + \
+            (arg2.var) / (math.pow(arg2.val, 2) * math.pow(math.log(arg2.val), 2)))
+    return result
 
 
 # Basic trigonometric functions
-def sin(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.sin(a.val)
-    c.var = math.pow(math.cos(a.val), 2) * a.var
-    return c
+def sin(arg1):
+    """
+    enerpy.sin:
+    Function to return sin a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.sin(arg1.val)
+    result.var = math.pow(math.cos(arg1.val), 2) * arg1.var
+    return result
 
-def cos(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.cos(a.val)
-    c.var = math.pow(math.sin(a.val), 2) * a.var
-    return c
+def cos(arg1):
+    """
+    enerpy.cos:
+    Function to return cos a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.cos(arg1.val)
+    result.var = math.pow(math.sin(arg1.val), 2) * arg1.var
+    return result
 
-def tan(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.tan(a.val)
-    c.var = math.pow(math.cos(a.val), -4) * a.var
-    return c
+def tan(arg1):
+    """
+    enerpy.tan:
+    Function to return tan a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.tan(arg1.val)
+    result.var = math.pow(math.cos(arg1.val), -4) * arg1.var
+    return result
 
 
 # Inverse trigonometric functions
-def arcsin(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.asin(a.val)
-    c.var = (1 / (1 - math.pow(a.val, 2))) * a.var
-    return c
+def arcsin(arg1):
+    """
+    enerpy.arcsin:
+    Function to return arcsin a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.asin(arg1.val)
+    result.var = (1 / (1 - math.pow(arg1.val, 2))) * arg1.var
+    return result
 
-def arccos(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.acos(a.val)
-    c.var = (1 / (1 - math.pow(a.val, 2))) * a.var
-    return c
+def arccos(arg1):
+    """
+    enerpy.arccos:
+    Function to return arccos a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.acos(arg1.val)
+    result.var = (1 / (1 - math.pow(arg1.val, 2))) * arg1.var
+    return result
 
-def arctan(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.atan(a.val)
-    c.var = (1 / math.pow(1 + math.pow(a.val, 2), 2)) * a.var
-    return c
+def arctan(arg1):
+    """
+    enerpy.arctan:
+    Function to return arctan a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.atan(arg1.val)
+    result.var = (1 / math.pow(1 + math.pow(arg1.val, 2), 2)) * arg1.var
+    return result
 
 
 # Hyperbolic trigonometric functions
-def sinh(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.sinh(a.val)
-    c.var = math.pow(math.cosh(a.val), 2) * a.var
-    return c
+def sinh(arg1):
+    """
+    enerpy.sinh:
+    Function to return sinh a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.sinh(arg1.val)
+    result.var = math.pow(math.cosh(arg1.val), 2) * arg1.var
+    return result
 
-def cosh(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.cosh(a.val)
-    c.var = math.pow(math.sinh(a.val), 2) * a.var
-    return c
+def cosh(arg1):
+    """
+    enerpy.cosh:
+    Function to return cosh a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.cosh(arg1.val)
+    result.var = math.pow(math.sinh(arg1.val), 2) * arg1.var
+    return result
 
-def tanh(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.tanh(a.val)
-    c.var = math.pow((2 * math.cosh(a.val)) / (math.cosh(2 * a.val) + 1), 2) * a.var
-    return c
+def tanh(arg1):
+    """
+    enerpy.tanh:
+    Function to return tanh a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.tanh(arg1.val)
+    result.var = math.pow((2 * math.cosh(arg1.val)) / \
+            (math.cosh(2 * arg1.val) + 1), 2) * arg1.var
+    return result
 
 
 # Inverse hyperbolic functions
-def arcsinh(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.asinh(a.val)
-    c.var = (1 / (1 + math.pow(a.val, 2))) * a.var
-    return c
+def arcsinh(arg1):
+    """
+    enerpy.arcsinh:
+    Function to return arcsinh a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.asinh(arg1.val)
+    result.var = (1 / (1 + math.pow(arg1.val, 2))) * arg1.var
+    return result
 
-def arccosh(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.acosh(a.val)
-    c.var = (1 / (math.pow(a.val, 2) - 1)) * a.var
-    return c
+def arccosh(arg1):
+    """
+    enerpy.arccosh:
+    Function to return arccosh a.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.acosh(arg1.val)
+    result.var = (1 / (math.pow(arg1.val, 2) - 1)) * arg1.var
+    return result
 
-def arctanh(a):
-    assert isinstance(a, Num)
-    c = Num()
-    c.val = math.atanh(a.val)
-    c.var = (1 / math.pow(1 - math.pow(a.val, 2), 2)) * a.var
-    return c
+def arctanh(arg1):
+    """
+    enerpy.arctanh:
+    Function to return arctanh a.
+    Error calculation is not correct, no idea why.
+    """
+    assert isinstance(arg1, Num)
+    result = Num()
+    result.val = math.atanh(arg1.val)
+    result.var = (1 / math.pow(1 - math.pow(arg1.val, 2), 2)) * arg1.var
+    return result
 
