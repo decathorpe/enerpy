eef77f91a4b472c28ed024de184ea35ad37055b0
Fabio Valentini <decathorpe@gmail.com>
2015-08-14 18:00:56 +0200

    add docstrings to enerpy/lists.py and make pylint happy in general.

--- enerpy/lists.py
+++ enerpy/lists.py
@@ -5,27 +5,35 @@ This module contains the class definition of a NumList.
 
 import math
 
-from enerpy.base import Num
-from enerpy.calc import *
-from enerpy.funcs import *
+from enerpy.base import Node, Num, enerfy
 
 
 class NumList(Node, list):
-    def __init__(self, nlist=[]):
+    """
+    enerpy.NumList:
+    Data class containing a list of Nums.
+    It is initialised with a list() of numbers, e. g. [1, 2] or [Num(2, 1), 1].
+    """
+    def __init__(self, nlist=None):
         super().__init__()
+        if nlist is None:
+            nlist = list()
         for i in nlist:
             self.append(i)
 
     def condense(self):
-        c = Num()
-        n = len(self)
+        """
+        enerpy.NumList().condense():
+        The .condense() method returns the mean and standard error of the list.
+        """
+        result = Num()
+        length = len(self)
 
         list_sumn = 0
         for i in self:
             list_sumn = list_sumn + enerfy(i).val
 
-        list_mean = list_sumn / n
-        c.val = list_mean
+        list_mean = list_sumn / length
 
         list_var = 0
         for i in self:
@@ -35,14 +43,20 @@ class NumList(Node, list):
         for i in self:
             valu_var = valu_var + enerfy(i).var
 
-        mean_vars = (1 / (n * (n - 1))) * list_var
-        valu_vars = (1 / math.pow(n, 2)) * valu_var
+        mean_vars = (1 / (length * (length - 1))) * list_var
+        valu_vars = (1 / math.pow(length, 2)) * valu_var
 
-        c.var = mean_vars + valu_vars
+        result.val = list_mean
+        result.var = mean_vars + valu_vars
 
-        return c
+        return result
 
     def prnt(self):
+        """
+        enerpy.NumList().prnt():
+        The .prnt() method prints the NumList contents in a human-readable way.
+        """
         for i in range(0, len(self)):
             print(str(i), ": ", end="")
             enerfy(self[i]).prnt()
+
